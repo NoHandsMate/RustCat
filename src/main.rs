037ba6bdd8388719ab8473635f7eb46bcb39a5ca
NoHandsMate <z170s.lol@gmail.com>
2021-03-01 23:20:43 +0100

    Added base functionality

--- src/main.rs
+++ src/main.rs
@@ -29,24 +29,65 @@ use std::env; // for collecting arguments
 use std::fs;  // File I/O
 
 
-fn parse_arguments(args : &[String]) -> (&str, &str) {
-
+fn parse_arguments(args : &[String]) -> (&str, &str, bool) {
+    
+    if args.len() < 3 {
+        
+        let file2_exists = false;
+        let file1 = &args[1];
+        let file2 = "";
+        
+        (file1, file2, file2_exists)
+    
+    } else {
+    
+    let file2_exists = true;
     let file1 = &args[1];
     let file2 = &args[2];
 
-    (file1, file2)
+    (file1, file2, file2_exists)
+
+    }
 }
+
+
+fn print_data(file : &str, file2 : &str) {
+    
+    println!("{}", file);
+    println!("{}", file2);
+}
+
 
 fn main() {
     
     let args : Vec<String> = env::args().collect();
 
-    let (file1, file2) = parse_arguments(&args);
+    let (file1, file2, exists) = parse_arguments(&args);
+    
+    if exists {
 
     let file1_content = fs::read_to_string(file1).expect("Something went wrong reading the first file");
     let file2_content = fs::read_to_string(file2).expect("Something went wrong reading the second file");
+    
+    print_data(&file1_content[..], &file2_content[..]);
 
-    println!("{}", file1_content);
-    println!("{}", file2_content);
+    } else {
+
+        let file1_content = fs::read_to_string(file1).expect("Something went wrong reading the first file");
+
+        print_data(&file1_content, "");
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
